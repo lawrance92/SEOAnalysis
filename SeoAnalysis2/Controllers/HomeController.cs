28de19d38229ad8ce1b22dd198770add926e9577
Khoghulan Vadiveloo <khoghulan@outlook.com>
2019-12-30 10:59:18 +0800

    Signed-off-by: Khoghulan Vadiveloo <khoghulan@outlook.com>

--- SeoAnalysis2/Controllers/HomeController.cs
+++ SeoAnalysis2/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web.Mvc;
-using HtmlAgilityPack;
+using HtmlAgilityPack; //Have install this nugget in project for filter out html metatag
 using SeoAnalysis2.Models;
 
 namespace SeoAnalysis2.Controllers
@@ -12,56 +12,65 @@ namespace SeoAnalysis2.Controllers
     {
         public ActionResult Index()
         {
+            // Return view for the web page from controller
             return View();
         }
         [HttpPost]
         public ActionResult CheckURL(HomeModel model)
         { 
+            //Check whether the model is in use 
             if (ModelState.IsValid)
             {
+                // Try to check the URL because some if the URL is not in use it will throw error or exception
                 try
                 {
                     //find out the URL existing with SYSTEM.NET library function
-                    var req = (HttpWebRequest)WebRequest.Create(model.URL);
-                    var res = (HttpWebResponse)req.GetResponse();
-                    var client = new WebClient();
-                    var stopWords = new List<string> { "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your"};
+                    var req = (HttpWebRequest)WebRequest.Create(model.URL); //Request web information from the web server
+                    var res = (HttpWebResponse)req.GetResponse(); //Get the response from web service
+                    var stopWords = new List<string> { "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your"}; // Assign string list for stop words
                    
                     //check whether it is valid website or not
                     if (res.StatusCode == HttpStatusCode.OK)
                     {
-                        var outWords = new List<string>();
-                        var web = new HtmlWeb();
-                        var doc = web.Load(model.URL);
+                        var outWords = new List<string>(); //New list string for store the similar string from stop-words
+                        var web = new HtmlWeb(); // Connect the HTMLAgilityPack for get the content on website URL
+                        var doc = web.Load(model.URL); //Load the websites
                         var text = doc.DocumentNode.Descendants()
                                   .Where(x => x.NodeType == HtmlNodeType.Text && x.InnerText.Trim().Length > 0)
-                                  .Select(x => x.InnerText.Trim());
+                                  .Select(x => x.InnerText.Trim()); //filter out the DOM document from website URL
 
-                        var urlContent = String.Join(" ", text);
+                        var urlContent = String.Join(" ", text); // join the string with spaces
                         
                         //loop for mach with stop words
                         foreach (var words in stopWords)
                         {
+                            // Condition for check the string match with stop-words or not
                             if (urlContent.IndexOf(words) > -1)
                             {
                                 outWords.Add(words);
                             }
                         }
 
+                        // Assign to model list field for view purposes
                         model.words = outWords;
 
+                        // Final will show that the website is in use
                         model.Exist = true;
                     }
                     else
                     {
+                        // It will identify the website is invalid
                         model.Exist = false;
                     }
                 }
                 catch (Exception)
                 {
+                    // It will identify the website is invalid
                     model.Exist = false;
                 }
             }
+
+            // return the model to the view
             return View("Index", model);
         }
     }
--- SeoAnalysis2/Models/HomeModel.cs
+++ SeoAnalysis2/Models/HomeModel.cs
@@ -9,9 +9,10 @@ namespace SeoAnalysis2.Models
 {
     public class HomeModel
     {
+        // Home model for store the information and pare to view to process
         [Required]
-        public string URL { get; set; }
-        public bool Exist { get; set; }
-        public List<string> words { get; set; }
+        public string URL { get; set; } // this field is required and cannot be empty
+        public bool Exist { get; set; } // Assign boolean value for website existing or not
+        public List<string> words { get; set; } // Assign the list of stop-words which is match with websites content
     }
 }
